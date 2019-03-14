b72b750c87e25fee09e7982b2c2d4287b855644a
David Gomez <dg.dave@gmail.com>
2019-03-14 13:30:53 -0400

    fix: extract getTransform function

--- packages/group/src/Group.js
+++ packages/group/src/Group.js
@@ -1,39 +1,27 @@
 // @flow
 import React from 'react';
-
-type CSS = {
-  map: string,
-  name: string,
-  styles: string,
-};
+import { getTransform } from '../../utils';
+import type { CSS, Transform } from '../../utils/types';
 
 type Props = {
   children: any,
   className?: string,
   css?: CSS,
-  transform?: {
-    left?: number,
-    top?: number,
-    rotation?: number | string,
-  },
+  transform?: Transform,
 };
 
 export default function Group({
   children,
   className = '',
   css,
-  transform = {},
+  transform,
   ...restProps
 }: Props) {
-  var { left, rotation, top } = transform;
-  var translate = left && top ? `translate(${left}, ${top})` : '';
-  var rotate = rotation ? `rotate(${rotation})` : '';
-
   return (
     <g
       className={`silky-group ${className}`}
       css={css}
-      transform={`${translate} ${rotate}`}
+      transform={getTransform(transform)}
       {...restProps}
     >
       {children}
